94b292d23a5ce6eb44142fd6ce7e8e858cde72db
Md. Sazzad Hossain <96966545+Sazz07@users.noreply.github.com>
2023-07-23 01:21:29 +0600

    added all blog post page

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
-// export const getBlogPost = async () => {
-//     try {
-//         const res = await fetch('https://dummyjson.com/posts?limit=30');
-//         const data = res.json();
-//         return data;
-//     }
-//     catch (error) {
-//         console.log(error);
-//     }
-// }
+export const getBlogPost = async () => {
+    try {
+        const res = await fetch('https://api.slingacademy.com/v1/sample-data/blog-posts?limit=30');
+        const data = res.json();
+        return data;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
--- app/blogs/page.jsx
+++ app/blogs/page.jsx
@@ -1,31 +1,50 @@
 'use client'
+// import { getBlogPost } from '@/api'
 import { useQuery } from '@tanstack/react-query'
-import React from 'react'
+import React, { useState } from 'react'
+import BlogCard from './components/BlogCard'
 
 const Blogs = () => {
-    // const {data} = useQuery({
-
+    // const { data: posts, isLoading } = useQuery({
+    //     queryKey: ['/blogs'],
+    //     queryFn: getBlogPost
     // })
+    const [showCard, setShowCard] = useState(8);
+    const { data: posts, isLoading } = useQuery({
+        queryKey: ['/blogs'],
+        queryFn: async () => {
+            try {
+                const res = await fetch('https://api.slingacademy.com/v1/sample-data/blog-posts?limit=30');
+                const data = res.json();
+                return data;
+            }
+            catch (error) {
+                console.log(error);
+            }
+        }
+    })
+
+    if (isLoading) {
+        return <h1>Loading..........</h1>
+    }
+
+    const { blogs } = posts;
+
+    const handleShowMore = () => {
+        setShowCard(prevShowCard => prevShowCard + 4);
+    }
+
     return (
         <div>
             <h1>All Blogs</h1>
             <div className="max-w-screen-xl mx-auto">
-                <div className="grid grid-cols-4">
-                    <div className="relative flex items-end justify-start w-full text-left bg-center bg-cover h-96 dark:bg-gray-500" style={{ backgroundImage: "url('https://source.unsplash.com/random/240x320')" }}>
-                        <div className="absolute top-0 left-0 right-0 bg-gradient-to-b via-transparent from-gray-900 to-gray-900"></div>
-                        <div className="absolute top-0 left-0 right-0 flex items-center justify-between mx-5 mt-3">
-                            <a rel="noopener noreferrer" href="#" className="px-3 py-2 text-xs font-semibold uppercase dark:text-gray-100 bgundefined">Politics</a>
-                            <div className="flex flex-col text-center dark:text-gray-100">
-                                <span className="text-3xl font-semibold">04</span>
-                                <span className="uppercase">Aug</span>
-                            </div>
-                        </div>
-                        <h2 className="z-10 p-5">
-                            <a href="#" className="font-medium text-md hover:underline text-gray-100">Autem sunt tempora mollitia magnam non voluptates</a>
-                        </h2>
-                    </div>
-                    {/* ... (other blog items with similar fixes) ... */}
+                <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-10">
+                    {
+                        blogs.slice(0, showCard).map(blog => <BlogCard key={blog.id}>
+                        </BlogCard>)
+                    }
                 </div>
+                {showCard < blogs.length && (<button onClick={handleShowMore}>Show More</button>)}
             </div>
         </div>
     )
